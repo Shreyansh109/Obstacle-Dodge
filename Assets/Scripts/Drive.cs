72f3881ea89050da714379b179a37bdded34e68d
Shreyansh <shreyanshgupta118@gmail.com>
2026-05-29 22:44:25 +0530

    minimum character development and movement system

---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    float xValue = 0f;
+    float zValue = 0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xValue = Input.GetAxis("Horizontal");
+        zValue = Input.GetAxis("Vertical");
+        print("xValue: " + xValue + " zValue: " + zValue);
+        transform.Translate(xValue * Time.deltaTime *10, 0f, zValue * Time.deltaTime *10);
+    }
+}
